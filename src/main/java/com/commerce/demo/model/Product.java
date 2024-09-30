ef5accc358ed2a286274a1aebc576c9816a125bf
adityapolicepatil <adityapolicepatil@gmail.com>
2024-09-30 23:24:01 +0530

    1stcommit

---
+++ src/main/java/com/commerce/demo/model/Product.java
@@ -0,0 +1,130 @@
+package com.commerce.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private int price;
+    private String name;
+    private String desc;
+
+    public Product() {
+    }
+
+    public Product(int id, int price, String name, String desc, String brand, String category, Date releaseDate, boolean available, int quantity) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+        this.desc = desc;
+        this.brand = brand;
+        this.category = category;
+        this.releaseDate = releaseDate;
+        this.available = available;
+        this.quantity = quantity;
+    }
+
+    @Override
+
+
+
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", price=" + price +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", brand='" + brand + '\'' +
+                ", category='" + category + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", available=" + available +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    private String brand;
+    private String category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+}
